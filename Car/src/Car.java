a601bc11ae26036590af1ea7866ccf8e8108b514
Rishabh Agarwal <rishaba@g.clemson.edu>
2017-09-11 01:22:51 -0600

    Object, Method, Classes and Data Types

---
+++ Car/src/Car.java
@@ -0,0 +1,40 @@
+/*Day 1*/
+public class Car {
+
+	int maxSpeed = 100;
+	int minSpeed = 0;
+	double weight = 4079; 
+	boolean isTheCarOn = false;
+	char condition = 'A';
+	
+	public void printVariables() {
+		System.out.println("This is the MaxSpeed: "+ maxSpeed);
+		System.out.println("This is the MinSpeed: "+ minSpeed);
+		System.out.println("This is the Weight: "+ weight);
+		System.out.println("Is the Car on: "+ isTheCarOn);
+		System.out.println("Condition of the Car: "+  condition);
+	}
+	
+	public void wreckCar() {
+		condition = 'C';
+	}
+	
+	public void upradeMinSpeed() {
+		minSpeed = maxSpeed;
+		maxSpeed = maxSpeed +1;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Car familyCar = new Car();
+        System.out.println("Family Car");
+        familyCar.printVariables();
+        /*Car aliceCar = familyCar; 
+        familyCar.wreckCar(); 
+        System.out.println("Alice Car");
+        aliceCar.printVariables();*/
+        familyCar.upradeMinSpeed();
+        familyCar.printVariables(); 
+	}
+
+}
