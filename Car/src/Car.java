73dbe6314278d3c05392c8618ac14b936f557d13
Rishabh Agarwal <rishaba@g.clemson.edu>
2017-09-11 01:57:02 -0600

    Day2 & Day 3

--- Car/src/Car.java
+++ Car/src/Car.java
@@ -1,12 +1,28 @@
-/*Day 1*/
+/*Introduction to construction*/
+
 public class Car {
 
 	int maxSpeed = 100;
 	int minSpeed = 0;
+	
 	double weight = 4079; 
+	
 	boolean isTheCarOn = false;
 	char condition = 'A';
 	String nameOfCar = "Lucy";
+	
+	double maxFuel = 16;
+	double currentFuel = 8;
+	double mpg = 26.4;
+	
+	int numberOfPeopleInCar = 1;
+	
+	//constructor is custom initializer- if we don't want default values
+	public Car(int customMinSpeed, String customCarName) {
+		minSpeed = customMinSpeed ;
+		nameOfCar = customCarName;
+	}
+	
 	
 	public void printVariables() {
 		System.out.println("This is the MaxSpeed: "+ maxSpeed);
@@ -15,6 +31,7 @@ public class Car {
 		System.out.println("Is the Car on: "+ isTheCarOn);
 		System.out.println("Condition of the Car: "+  condition);
 		System.out.println("Name of Car: "+  nameOfCar);
+		System.out.println("Number Of People in Car: "+ numberOfPeopleInCar);
 	}
 	
 	public void wreckCar() {
@@ -26,17 +43,24 @@ public class Car {
 		maxSpeed = maxSpeed +1;
 	}
 	
+	public void getIn() {
+		numberOfPeopleInCar = numberOfPeopleInCar+1;
+	}
+	
+	public void getOut() {
+		numberOfPeopleInCar --;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        Car familyCar = new Car();
-        System.out.println("Family Car");
-        familyCar.printVariables();
-        /*Car aliceCar = familyCar; 
-        familyCar.wreckCar(); 
-        System.out.println("Alice Car");
-        aliceCar.printVariables();*/
-        familyCar.upradeMinSpeed();
-        familyCar.printVariables(); 
+		
+		Car BirthdayPresent  = new Car(10, "BirthdayPresent");
+		BirthdayPresent.getIn();
+		BirthdayPresent.getIn();
+		BirthdayPresent.printVariables();
+		Car christmasPresent = new Car(550,"christmasPresent");
+		christmasPresent.getOut();
+		christmasPresent.printVariables();
+		
 	}
 
 }
