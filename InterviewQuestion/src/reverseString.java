f7f1ede900780f7faa1ee37eef754558fc50fa27
Rishabh Agarwal <rishaba@g.clemson.edu>
2017-09-19 17:45:18 -0600

    Reverse a String Program

---
+++ InterviewQuestion/src/reverseString.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class reverseString {
+	
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Enter the String to be reversed");
+		String inputString = sc.nextLine();
+	}
+}
