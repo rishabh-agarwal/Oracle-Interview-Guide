bdc408d5e797649a8354cfbdf4d6f2a72894a9d6
Rishabh Agarwal <rishaba@g.clemson.edu>
2017-09-19 16:06:32 -0600

    HashMap

---
+++ CollectionPractice/src/DictionaryPractice.java
@@ -0,0 +1,25 @@
+/*Map Example*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DictionaryPractice {
+
+	public static void main(String[] args)
+	{
+		// English to Spanish Dictionary
+		Map<String,String> englSpanDictionary = new HashMap<String,String>();
+		// Putting things inside our dictionary
+		englSpanDictionary.put("Monday","Lunes");
+		englSpanDictionary.put("Tueday", "Martes"); 
+		englSpanDictionary.put("Wednesday", "Miercoles");
+		englSpanDictionary.put("Thrusday", "Jueves");
+		englSpanDictionary.put("Friday", "Viernes");
+		// Retrieve things from our dictionary
+		System.out.println(englSpanDictionary.get("Monday"));
+		System.out.println(englSpanDictionary.get("Tuesday"));
+		System.out.println(englSpanDictionary.get("Wednesday"));
+		System.out.println(englSpanDictionary.get("Thrusday"));
+		System.out.println(englSpanDictionary.get("Friday"));
+	}
+}
