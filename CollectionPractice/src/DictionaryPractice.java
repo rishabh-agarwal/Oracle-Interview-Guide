b88e367ae647f81907bc2c8703da43481891766d
Rishabh Agarwal <rishaba@g.clemson.edu>
2017-09-19 16:34:49 -0600

    HashMap

--- CollectionPractice/src/DictionaryPractice.java
+++ CollectionPractice/src/DictionaryPractice.java
@@ -15,6 +15,8 @@ public class DictionaryPractice {
 		englSpanDictionary.put("Wednesday", "Miercoles");
 		englSpanDictionary.put("Thrusday", "Jueves");
 		englSpanDictionary.put("Friday", "Viernes");
+		englSpanDictionary.put("Saturday", "sabado");
+		englSpanDictionary.put("Sunday", "Domingo");
 		// Retrieve things from our dictionary
 		System.out.println(englSpanDictionary.get("Monday"));
 		System.out.println(englSpanDictionary.get("Tuesday"));
@@ -24,5 +26,38 @@ public class DictionaryPractice {
 		
 		System.out.println(englSpanDictionary.keySet());
 		System.out.println(englSpanDictionary.values());
+		
+		System.out.println("The Size of our Dictionary is: "+englSpanDictionary.size());
+		
+		System.out.println();
+		System.out.println();
+		
+		Map<String, Boolean> shoppingList = new HashMap<String, Boolean>();
+		//why we can't write small boolean, need to be capitalize as there is 
+		//no reference to it, capitalizing it make a object to boolean
+		
+		shoppingList.put("Ham", true);
+		shoppingList.put("Bread", Boolean.TRUE);
+		shoppingList.put("Oreos", Boolean.TRUE);
+		shoppingList.put("Eggs", Boolean.FALSE);
+		shoppingList.put("Sugar", false);
+		
+		//Retrieve item
+		System.out.println(shoppingList.get("Ham"));
+		System.out.println(shoppingList.get("Oreos"));
+		System.out.println(shoppingList.toString());
+		
+		//Remove things
+		shoppingList.remove("Eggs");
+		System.out.println("Eggs Remove:"+shoppingList.toString());
+		
+		//Replace value
+		shoppingList.replace("Bread", Boolean.FALSE);
+		System.out.println("Bread Replace:"+shoppingList.toString());
+		// Key Value Pairs Print Out
+		shoppingList.clear();
+		System.out.println(shoppingList.toString());
+		System.out.println(shoppingList.isEmpty());
+	
 	}
 }
