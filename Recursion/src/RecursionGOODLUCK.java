9f0aa1ec6da5748461cee75fda96ec02d6d686bb
Rishabh Agarwal <rishaba@g.clemson.edu>
2017-09-22 18:00:14 -0600

    small changes

--- Recursion/src/RecursionGOODLUCK.java
+++ Recursion/src/RecursionGOODLUCK.java
@@ -10,8 +10,19 @@ public class RecursionGOODLUCK {
 			return n + Summation(n-1);
 			}
 	}
+	 
+	public static int Factorial(int n) {
+		if(n<=1) {
+			return 1;
+		} 
+		else {
+		
+			return n * Factorial(n-1);
+		}
+	}
 
 	public static void main(String[] args) {
-		System.out.println(Summation(3));
+		System.out.println("Summation "+Summation(10));
+		System.out.println("Factorial "+Factorial(10));
 	}
 }
